fce7ec31ba9dfa6f5423cf92402ddf2804712853
Vitordev539 <vitorcarlossilva62@gmail.com>
2025-02-24 04:08:38 +0000

    inicei a criação do jogo

---
+++ supertrunfo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    char carta1 ;
+    char nome_estado, codigo_carta [50];
+    int populacao, numero_pontos_turisticos;
+    float area_km, pib;
+
+    char carta2 ;
+    char nome_estado2, codigo_estado2 [50];
+    int populacao2, numero_pontos_turisticos2;
+    float area_km2, pib2;
+}
