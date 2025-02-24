ab3e5d6c6fd87f6f0a51582214eebbddaf2f098b
Vitordev539 <vitorcarlossilva62@gmail.com>
2025-02-24 05:12:18 +0000

    Desafio concluído

--- supertrunfo.c
+++ supertrunfo.c
@@ -2,16 +2,16 @@
 
 int main()
 {
-    //Declarando o tipo de variavéis
-    char carta1 ;
-    char nome_estado [50];
-    int populacao,codigo_carta, numero_pontos_turisticos;
+    // Declarando o tipo de variavéis da carta 1
+    char carta1;
+    char nome_estado[50];
+    int populacao, codigo_carta, numero_pontos_turisticos;
     float area_km, pib;
-
-    printf("Digite o nome do estado: \n ");
+    // Solicitando ao usuário os dados da carta 1
+    printf("Digite o nome do estado: \n");
     scanf("%s", nome_estado);
 
-    printf("Digite o codigo da carta: \n ");
+    printf("Digite o codigo da carta: \n");
     scanf("%d", &codigo_carta);
 
     printf("Digite a população: \n");
@@ -26,21 +26,15 @@ int main()
     printf("Digite o valor do pib: \n");
     scanf("%f", &pib);
 
-    printf("A carta 1 tem as seguintes informações:\n");
-    printf("Nome do estado: %s\nCódigo da carta: %d \n", nome_estado, codigo_carta);
-
-    printf("População: %d\nNumero de pontos turístios: %d \n", populacao, numero_pontos_turisticos);
-    printf("Área em km: %f\nPib: %f \n", area_km, pib);
-
-    char carta2 ;
-    char nome_estado2 [50];
-    int populacao2,codigo_carta2, numero_pontos_turisticos2;
+    char carta2;
+    char nome_estado2[50];
+    int populacao2, codigo_carta2, numero_pontos_turisticos2;
     float area_km2, pib2;
-    
-    printf("Digite o nome do estado: \n ");
+    // Solicitando ao usuário os dados da carta 2
+    printf("Digite o nome do estado: \n");
     scanf("%s", nome_estado2);
 
-    printf("Digite o codigo da carta: \n ");
+    printf("Digite o codigo da carta: \n");
     scanf("%d", &codigo_carta2);
 
     printf("Digite a população: \n");
@@ -54,19 +48,18 @@ int main()
 
     printf("Digite o valor do pib: \n");
     scanf("%f", &pib2);
-
+    // Exibindo em tela os dados solitados anteriormente da carta 1
     printf("A carta 1 tem as seguintes informações:\n");
     printf("Nome do estado: %s\nCódigo da carta: %d \n", nome_estado, codigo_carta);
 
     printf("População: %d\nNumero de pontos turístios: %d \n", populacao, numero_pontos_turisticos);
     printf("Área em km: %f\nPib: %f \n", area_km, pib);
-
+    // Exibindo em tela os dados solitados anteriormente da carta 2
     printf("A carta 2 tem as seguintes informações:\n");
     printf("Nome do estado: %s\nCódigo da carta: %d \n", nome_estado2, codigo_carta2);
 
     printf("População: %d\nNumero de pontos turístios: %d \n", populacao2, numero_pontos_turisticos2);
     printf("Área em km: %f\nPib: %f \n", area_km2, pib2);
 
-
-     
+    return 0;
 }
